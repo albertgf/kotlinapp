a928408ed5a38229cfe6a8f58fb48f0e54a7673f
albert <albertgfdev@gmail.com>
2017-09-05 17:12:47 +0200

    chapter 13

---
+++ app/src/main/java/com/bendroid/kotlinapp/ui/utils/ViewExtensions.kt
@@ -0,0 +1,10 @@
+package com.bendroid.kotlinapp.ui.utils
+
+import android.content.Context
+import android.view.View
+
+/**
+ * Created by albert on 05/09/2017.
+ */
+val View.ctx: Context
+    get() = context
